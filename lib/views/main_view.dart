6a3384a72476f02a33286c09a1021cdf0d5f28e9
Anastasiia Tsapko <anastsapko@gmail.com>
2023-12-21 18:03:11 +0200

    Fixed list scrolling

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -38,7 +38,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffd9d9d9),
+        backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: const Color(0xfff0f424),
           title: const Text(
@@ -52,54 +52,59 @@ class _MainPageState extends State<MainPage> {
         ),
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 150,
-                child: TextField(
-                  controller: _textController,
-                  maxLines: null,
-                  expands: true,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: 'Start writting your todo...',
-                  ),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
+            Container(
+              color: Colors.white,
+              child: Column(children: [
                 Padding(
-                  padding: const EdgeInsets.only(right: 15.0),
-                  child: ElevatedButton(
-                    onPressed: isTextEmpty
-                        ? null
-                        : () {
-                            items.add(_textController.text);
-                            _textController.clear();
-                          },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFF0F424)),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.black54,
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 100,
+                    child: TextField(
+                      controller: _textController,
+                      maxLines: null,
+                      expands: true,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Start writting your todo...',
+                      ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            const Text(
-              'Your todos',
-              style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 15.0),
+                      child: ElevatedButton(
+                        onPressed: isTextEmpty
+                            ? null
+                            : () {
+                                items.add(_textController.text);
+                                _textController.clear();
+                              },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFF0F424)),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  'Your todos',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold),
+                ),
+              ]),
             ),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(8.0),
                 child: ListView.builder(
                   itemCount: items.length,
                   itemBuilder: (context, index) {
@@ -160,6 +165,10 @@ class _MainPageState extends State<MainPage> {
                 ),
               ),
             ),
+            Container(
+              color: Colors.white,
+              height: 20,
+            )
           ],
         ));
   }
